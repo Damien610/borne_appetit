a459e0315a7aedc4ac06fc5cee5eafb68f057ebf
Damien Nerriere <nerridamien@gmail.com>
2026-02-04 22:30:39 +0100

    Enable S3 backend - infrastructure ready

--- app/tf/terraform.tf
+++ app/tf/terraform.tf
@@ -8,14 +8,13 @@ terraform {
     }
   }
 
-  # Backend S3 - Temporairement commenté
-  # backend "s3" {
-  #   bucket         = "borne-appetit-terraform-state"
-  #   key            = "terraform.tfstate"
-  #   region         = "eu-west-1"
-  #   dynamodb_table = "borne-appetit-terraform-locks"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "borne-appetit-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "borne-appetit-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
