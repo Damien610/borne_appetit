29159e80cf9c7a9672e2d1e1cab19ca91f582cdd
Damien Nerriere <nerridamien@gmail.com>
2026-02-04 21:17:04 +0100

    Fix duplicate resources

--- app/tf/outputs.tf
+++ app/tf/outputs.tf
@@ -28,25 +28,6 @@ output "dynamodb_table_arn" {
   description = "ARN de la table DynamoDB"
   value       = aws_dynamodb_table.borne_appetit.arn
 }
-output "cloudfront_domain_name" {
-  description = "URL CloudFront"
-  value       = aws_cloudfront_distribution.cdn.domain_name
-}
-
-output "cloudfront_distribution_id" {
-  description = "ID de la distribution CloudFront"
-  value       = aws_cloudfront_distribution.cdn.id
-}
-
-output "s3_bucket_name" {
-  description = "Nom du bucket S3"
-  value       = aws_s3_bucket.assets.id
-}
-
-output "s3_images_bucket_name" {
-  description = "Nom du bucket S3 pour les images"
-  value       = aws_s3_bucket.images.id
-}
 
 output "aws_region" {
   description = "Région AWS utilisée"
