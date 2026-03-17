0288da4d89d501212113a93682e758e2330f24ed
Damien Nerriere <nerridamien@gmail.com>
2026-03-17 14:26:16 +0100

    fix pipeline

--- app/tf/modules/lambda/layer.tf
+++ app/tf/modules/lambda/layer.tf
@@ -1,27 +1,9 @@
-# Lambda Layer - Build automatique avec Docker
-resource "null_resource" "build_shared_layer" {
-  triggers = {
-    infrastructure = filemd5("${path.module}/../../../src/layers/infrastructure/dynamodb_repositories.py")
-    application    = filemd5("${path.module}/../../../src/layers/application/use_cases.py")
-    domain         = filemd5("${path.module}/../../../src/layers/domain/customer.py")
-    requirements   = filemd5("${path.module}/../../../src/lambda/wallet/requirements.txt")
-  }
-  
-  provisioner "local-exec" {
-    command = <<-EOT
-      cd ${path.module}/../../..
-      bash build_shared_layer.sh
-    EOT
-  }
-}
-
+# Lambda Layer
 resource "aws_lambda_layer_version" "shared_layer" {
   filename            = "${path.module}/../../../src/lambda/shared_layer.zip"
   layer_name          = "${var.project_name}-shared-layer"
   compatible_runtimes = ["python3.11"]
   source_code_hash    = filebase64sha256("${path.module}/../../../src/lambda/shared_layer.zip")
-  
-  depends_on = [null_resource.build_shared_layer]
 }
 
 output "shared_layer_arn" {
